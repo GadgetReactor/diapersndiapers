4d8a6dec96ef5c44a7f1b4f3d18eec297fcd3130
GadgetReactor <sean@gadgetreactor.com>
2015-08-14 18:15:06 +0800

    Update Countdown

--- js/init.js
+++ js/init.js
@@ -8,37 +8,37 @@ jQuery(document).ready(function($) {
 
 /*---------------------------------------------------- */
 /* Preloader
------------------------------------------------------- */ 
+------------------------------------------------------ */
   	$(window).load(function() {
 
-   	// will first fade out the loading animation 
-    	$("#status").fadeOut("slow"); 
+   	// will first fade out the loading animation
+    	$("#status").fadeOut("slow");
 
-    	// will fade out the whole DIV that covers the website. 
-    	$("#preloader").delay(500).fadeOut("slow").remove();      
+    	// will fade out the whole DIV that covers the website.
+    	$("#preloader").delay(500).fadeOut("slow").remove();
 
-  	}) 
+  	})
 
 /*---------------------------------------------------- */
 /* Menu
------------------------------------------------------- */  
-  	var toggle_button = $("<a>", {                         
-                        id: "toggle-btn", 
+------------------------------------------------------ */
+  	var toggle_button = $("<a>", {
+                        id: "toggle-btn",
                         html : "Menu",
                         title: "Menu",
-                        href : "#" } 
+                        href : "#" }
                         );
   	var nav_wrap = $('nav#nav-wrap')
-  	var nav = $("ul#nav");  
+  	var nav = $("ul#nav");
 
-  	/* id JS is enabled, remove the two a.menu-btns 
+  	/* id JS is enabled, remove the two a.menu-btns
   	and dynamically prepend a.toggle-btn to #nav-wrap */
-  	nav_wrap.find('a.menu-btn').remove(); 
-  	nav_wrap.prepend(toggle_button); 
+  	nav_wrap.find('a.menu-btn').remove();
+  	nav_wrap.prepend(toggle_button);
 
   	toggle_button.on("click", function(e) {
    	e.preventDefault();
-    	nav.slideToggle("fast");     
+    	nav.slideToggle("fast");
   	});
 
   	if (toggle_button.is(':visible')) nav.addClass('mobile');
@@ -47,8 +47,8 @@ jQuery(document).ready(function($) {
     	else nav.removeClass('mobile');
   	});
 
-  	$('ul#nav li a').on("click", function(){      
-   	if (nav.hasClass('mobile')) nav.fadeOut('fast');      
+  	$('ul#nav li a').on("click", function(){
+   	if (nav.hasClass('mobile')) nav.fadeOut('fast');
   	});
 
 
@@ -61,23 +61,23 @@ jQuery(document).ready(function($) {
 
 /*----------------------------------------------------*/
 /*  Placeholder Plugin Settings
------------------------------------------------------- */  	 
+------------------------------------------------------ */
 	$('input, textarea').placeholder()
 
 /*----------------------------------------------------*/
 /* Final Countdown Settings
 ------------------------------------------------------ */
-	var finalDate = '2015/03/01';
+	var finalDate = '2015/10/01';
 
 	$('div#counter').countdown(finalDate)
    	.on('update.countdown', function(event) {
 
-   		$(this).html(event.strftime('<span>%D <em>days</em></span>' + 
-   										 	 '<span>%H <em>hours</em></span>' + 
+   		$(this).html(event.strftime('<span>%D <em>days</em></span>' +
+   										 	 '<span>%H <em>hours</em></span>' +
    										 	 '<span>%M <em>minutes</em></span>' +
    										 	 '<span>%S <em>seconds</em></span>'));
 
-   });  
+   });
 
 
 /*----------------------------------------------------*/
